d82a80500f4e03dde45909b809adfe330fede405
lecocq <sgfdr>
2018-04-16 09:38:37 +0200

    création de l'entité menu

---
+++ src/Entity/Menu.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MenuRepository")
+ */
+class Menu
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+ * @ORM\Column(type="string", length=50)
+ */
+    private $name;
+
+    /**
+    * @ORM\Column(type="float")
+    */
+    private $price;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+}
