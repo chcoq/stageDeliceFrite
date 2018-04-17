3c94530db4a67343eea99df9d98cbefedeb8bbc0
lecocq <sgfdr>
2018-04-17 15:32:28 +0200

    Ajout de la page Américain avec affichage des produits grace à un ManyToMany

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -27,21 +27,37 @@ class AppController extends  Controller
             'cats'=>$cats
         ]);
     }
-        /**
+
+    /**
      * @Route("/seul", name="seul")
      */
 
     public function seul(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-//        $menus = $em->getRepository('App:Menu')->findBy(['name'=>'Fricadelle']);
         $menus = $em->getRepository('App:Menu')->findAll();//Select * from Menu
         $cats = $em->getRepository('App:Category')->findAll();//Select * from Menu
-
-
         return $this-> render('/seul.html.twig',[
             'menus'=>$menus,
             'cats'=>$cats
+        ]);
+    }
+
+    /**
+     * @Route("/americain", name="americain")
+     */
+
+    public function americain(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $menus = $em->getRepository('App:Menu')->findAll();//Select * from Menu
+        $cats = $em->getRepository('App:Category')->findAll();//Select * from Menu
+        $prods = $em->getRepository('App:Product')->findAll();
+
+        return $this-> render('/americain.html.twig',[
+            'menus'=>$menus,
+            'cats'=>$cats,
+            'prods'=>$prods
         ]);
     }
 
