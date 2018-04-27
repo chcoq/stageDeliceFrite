c14584e570ed59ba455c98037b7f63057e0c5cb3
lecocq <sgfdr>
2018-04-27 14:59:05 +0200

    Route avant Menu Route  maintenant Accueil

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lecocq
+ * Date: 17/04/2018
+ * Time: 15:48
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends Controller
+{
+    /**
+     * @Route("/menu", name="menu")
+     */
+    public function menu(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $menus = $em->getRepository('App:Menu')->findAll();//Select * from Menu
+        $cats = $em->getRepository('App:Category')->findAll();//Select * from Menu
+
+
+        return $this->render('/menu.html.twig', [
+            'menus' => $menus,
+            'cats' => $cats
+        ]);
+    }
+
+    /**
+     * @Route("/seul", name="seul")
+     */
+    public function seul(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $menus = $em->getRepository('App:Menu')->findAll();//Select * from Menu
+        $cats = $em->getRepository('App:Category')->findAll();//Select * from Menu
+        return $this-> render('/seul.html.twig',[
+            'menus'=>$menus,
+            'cats'=>$cats
+        ]);
+    }
+
+    /**
+     * @Route("/americain", name="americain")
+     */
+    public function americain(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $menus = $em->getRepository('App:Menu')->findAll();//Select * from Menu
+        $cats = $em->getRepository('App:Category')->findAll();//Select * from Menu
+        $prods = $em->getRepository('App:Product')->findAll();
+
+        return $this-> render('/americain.html.twig',[
+            'menus'=>$menus,
+            'cats'=>$cats,
+            'prods'=>$prods
+        ]);
+    }
+
+    /**
+     * @Route("/sandwich", name="sandwich")
+     */
+    public function sandwich(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $menus = $em->getRepository('App:Menu')->findAll();//Select * from Menu
+        $cats = $em->getRepository('App:Category')->findAll();//Select * from Menu
+        $prods = $em->getRepository('App:Product')->findAll();
+
+        return $this-> render('/sandwich.html.twig',[
+            'menus'=>$menus,
+            'cats'=>$cats,
+            'prods'=>$prods
+        ]);
+    }
+    /*
+     * SELECT name_cat, name, price FROM menu INNER JOIN category on category.id = menu.category_id WHERE name_cat='seul'
+     */
+}
